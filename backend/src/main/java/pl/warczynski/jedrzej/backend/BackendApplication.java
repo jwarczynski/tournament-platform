aef50d75b6247e87783eb2c927b77dea4c435877
Jędrzej Warczyński <jedrzej.warczynski@comarch.com>
2023-05-03 11:28:30 +0200

    add transaction management possibility

--- backend/src/main/java/pl/warczynski/jedrzej/backend/BackendApplication.java
+++ backend/src/main/java/pl/warczynski/jedrzej/backend/BackendApplication.java
@@ -1,20 +1,15 @@
 package pl.warczynski.jedrzej.backend;
 
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-//import com.mongodb.client.MongoClient;
-//import com.mongodb.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.security.SecureRandom;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class BackendApplication implements WebMvcConfigurer {
@@ -29,18 +24,10 @@ public class BackendApplication implements WebMvcConfigurer {
     registry.addMapping("/**").allowedOrigins("*").allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS");
   }
 
-//  @Bean
-//  public MongoTemplate mongoTemplate() {
-//    MongoClientSettings settings = MongoClientSettings.builder()
-//            .applyToClusterSettings(builder ->
-//                    builder.hosts(Arrays.asList(new ServerAddress("<cluster-address>", 27017))))
-//            .credential(MongoCredential.createCredential("<username>", "<database>", "<password>".toCharArray()))
-//            .build();
-//
-//    MongoClient mongoClient = MongoClients.create(settings);
-//    return new MongoTemplate(mongoClient, "tournament-platform");
-//  }
-
+  @Bean
+  MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+    return new MongoTransactionManager(dbFactory);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(BackendApplication.class, args);
