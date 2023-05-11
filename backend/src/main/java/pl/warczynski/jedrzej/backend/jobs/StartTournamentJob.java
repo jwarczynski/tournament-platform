dfcd9a0ea1249bb6b7f3ab3d5440c585094815ec
Jędrzej Warczyński <jedrzej.warczynski@comarch.com>
2023-05-11 21:14:31 +0200

    fix: restriction of neroller players in case of updating tournament

--- backend/src/main/java/pl/warczynski/jedrzej/backend/jobs/StartTournamentJob.java
+++ backend/src/main/java/pl/warczynski/jedrzej/backend/jobs/StartTournamentJob.java
@@ -2,17 +2,19 @@ package pl.warczynski.jedrzej.backend.jobs;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import pl.warczynski.jedrzej.backend.models.tournament.Tournament;
+import pl.warczynski.jedrzej.backend.models.tournament.players.Player;
 import pl.warczynski.jedrzej.backend.services.impl.util.DuelsDrawer;
 import pl.warczynski.jedrzej.backend.services.interfaces.DuelService;
 import pl.warczynski.jedrzej.backend.services.interfaces.TournamentService;
 
-@Component
+import java.util.List;
+
+import static pl.warczynski.jedrzej.backend.jobs.scheduler.SchedulerConfig.TOURNAMENT_CONTEXT_KEY;
+
 public class StartTournamentJob implements Job {
     private static final Logger logger = LoggerFactory.getLogger(StartTournamentJob.class);
     @Autowired
@@ -25,11 +27,18 @@ public class StartTournamentJob implements Job {
     public StartTournamentJob() {}
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        String tournamentId = (String) context.getJobDetail().getJobDataMap().get("tournamentId");
+    public void execute(JobExecutionContext context) {
+        String tournamentId = (String) context.getJobDetail().getJobDataMap().get(TOURNAMENT_CONTEXT_KEY);
         Tournament tournament = tournamentService.getTournamentById(tournamentId).orElse(null);
 
-        var duels = duelsDrawer.generateDuels(tournament.getEnrolledPlayers(),
+        assert tournament != null;
+        List<Player> enrolledPlayers = tournament.getEnrolledPlayers();
+        int registrationLimit = tournament.getRegistrationLimit();
+        if (enrolledPlayers.size() > registrationLimit) {
+            enrolledPlayers = enrolledPlayers.subList(0, registrationLimit);
+        }
+
+        var duels = duelsDrawer.generateDuels(enrolledPlayers,
                 tournament.get_id(), tournament.getSeedPlayers());
         duelService.saveDuels(duels);
 
