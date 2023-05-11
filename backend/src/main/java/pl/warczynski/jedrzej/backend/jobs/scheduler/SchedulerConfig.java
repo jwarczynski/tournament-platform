8a461e3540e258bd02e0e3e20edf2e83b90b89eb
Jędrzej Warczyński <jedrzej.warczynski@comarch.com>
2023-05-11 21:10:40 +0200

    fix: creating jobs

--- backend/src/main/java/pl/warczynski/jedrzej/backend/jobs/scheduler/SchedulerConfig.java
+++ backend/src/main/java/pl/warczynski/jedrzej/backend/jobs/scheduler/SchedulerConfig.java
@@ -11,6 +11,9 @@ import java.util.Date;
 
 @Component
 public class SchedulerConfig {
+    public static final String TOURNAMENT_CONTEXT_KEY = "tournament";
+    public static final String START_TOURNAMENT_JOB_PREFIX = "startTournamentJob_";
+    private static final String TOURNAMENT_JOB_GROUP = "startTournamentJobGroup";
     private static final String START_TOURNAMENT_TRIGGER_GROUP = "StartTournamentTriggerGroup";
 
     private final Scheduler scheduler;
@@ -29,22 +32,30 @@ public class SchedulerConfig {
     }
 
     private boolean jobExists(String tournamentId) throws SchedulerException {
-        JobKey jobKey = new JobKey("startTournamentJob_" + tournamentId, "startTournamentJobGroup");
+        JobKey jobKey = new JobKey(START_TOURNAMENT_JOB_PREFIX + tournamentId, TOURNAMENT_JOB_GROUP);
         return scheduler.checkExists(jobKey);
     }
 
     private void rescheduleJob(String tournamentId, Date startDate) throws SchedulerException {
+        JobKey jobKey = new JobKey(START_TOURNAMENT_JOB_PREFIX + tournamentId, TOURNAMENT_JOB_GROUP);
+        JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+        JobDataMap jobDataMap = jobDetail.getJobDataMap();
+        jobDataMap.put(TOURNAMENT_CONTEXT_KEY, tournamentId);
+
         Trigger newTrigger = TriggerBuilder.newTrigger()
                 .withIdentity(tournamentId, START_TOURNAMENT_TRIGGER_GROUP)
                 .startAt(DateUtils.addMinutes(new Date(), 1))
+                .forJob(jobDetail)
                 .build();
         scheduler.rescheduleJob(TriggerKey.triggerKey(tournamentId, START_TOURNAMENT_TRIGGER_GROUP), newTrigger);
     }
 
+
     private void createJob(String tournamentId, Date startDate) throws SchedulerException {
         JobDetail jobDetail = JobBuilder.newJob(StartTournamentJob.class)
-                .withIdentity("startTournamentJob_" + tournamentId, "startTournamentJobGroup")
+                .withIdentity(START_TOURNAMENT_JOB_PREFIX + tournamentId, TOURNAMENT_JOB_GROUP)
                 .build();
+        jobDetail.getJobDataMap().put(TOURNAMENT_CONTEXT_KEY, tournamentId);
 
         Trigger trigger = TriggerBuilder.newTrigger()
                 .withIdentity(tournamentId, START_TOURNAMENT_TRIGGER_GROUP)
